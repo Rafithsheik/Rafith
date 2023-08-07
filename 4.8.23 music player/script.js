7993c76b49d3707370060aab918d021219d65044
Rafith Sheik <114178044+Rafithsheik@users.noreply.github.com>
2023-08-07 10:06:20 +0530

    Add files via upload

---
+++ 4.8.23 music player/script.js
@@ -0,0 +1,5 @@
+//let e=document.getElementById("prev")
+//let f=document.getElementById("play")
+//let g=document.getElementById("next")
+//let k=["music"]
+//let x=k.length;
---
+++ 4.8.23 music player/test/script.js
@@ -0,0 +1,3 @@
+a=document.getElementById("increment-btn")
+b=document.getElementById("counter-value")
+c=document.getElementById("decrement-btn")
